04d5590a0c0190cf08a3fc68817b7cff786ee821
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-09-19 17:51:47 +0600

    search hit complete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
+// https://covers.openlibrary.org/b/id/554106-M.jpg
+// https://covers.openlibrary.org/b/id/{cover_i}-M.jpg
 document.querySelector('#button-addon2').addEventListener('click', function () {
-    const searchText = document.querySelector('#search-input').value;
-    console.log(searchText);
+    let searchText = document.querySelector('#search-input').value;
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    searchText.value = '';
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadBooks(data));
 });
+
+const loadBooks = (data) => {
+    console.log(data)
+    showSearchHit(data);
+}
+
+const showSearchHit = (data) => {
+    document.querySelector('#search-hits').innerText = `${data.numFound} hits`;
+}
