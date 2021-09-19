d636cbfd6c5568766b8216eeb96f207ad4cc0f78
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-09-19 17:23:16 +0600

    search text complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-alert('Bismillah')
+document.querySelector('#button-addon2').addEventListener('click', function () {
+    const searchText = document.querySelector('#search-input').value;
+    console.log(searchText);
+});
