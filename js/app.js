a21804b7d72d30b0f9a75ee409873d3d95a204fa
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-09-19 19:57:33 +0600

    search results showing in html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-// https://covers.openlibrary.org/b/id/554106-M.jpg
-// https://covers.openlibrary.org/b/id/{cover_i}-M.jpg
-document.querySelector('#button-addon2').addEventListener('click', function () {
+document.querySelector('#button-addon2').addEventListener('click', () => {
     let searchText = document.querySelector('#search-input').value;
-
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     searchText.value = '';
 
@@ -12,10 +9,29 @@ document.querySelector('#button-addon2').addEventListener('click', function () {
 });
 
 const loadBooks = (data) => {
-    console.log(data)
     showSearchHit(data);
+    showResults(data);
 }
 
 const showSearchHit = (data) => {
     document.querySelector('#search-hits').innerText = `${data.numFound} hits`;
+}
+
+const showResults = (data) => {
+    data.docs.forEach(book => {
+        const searchResult = document.querySelector('#search-result');
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" id="book-cover" class="card-img-top" alt="Book Cover">
+                <div class="card-body">
+                    <h4 id="book-title" class="card-title">${book.title}</h4>
+                    <h6 id="book-author" class="card-text">by ${book.author_name}</h6>
+                    <small id="first-published">First published in ${book.first_publish_year}</small>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
 }
